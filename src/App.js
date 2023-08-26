7d131c61ae8257929be8631ff63b853f4ce85cc3
Fabiano Fuzetto <fabianofuzetto@gmail.com>
2023-08-26 00:46:18 -0300

    ,

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 import TelaCadastroPatrimonio from "./telas/TelaCadastroPatrimonio.jsx";
 import TelaCadastrocategoria from "./telas/TelaCadastroCategoria.jsx";
-import TelaCadastrofuncao from "./telas/TelaCadastroFuncao.jsx";
-import TelaCadastroevento from "./telas/TelaCadastroEvento.jsx";
-import TelaCadPessoa from "./telas/TelaCadPessoa.jsx";
-import TelaCadastroFinanceiro from "./telas/TelaCadastroFinanceiro.jsx";
 import TelaMenu from "./telas/TelaMenu";
 import Tela404 from "./telas/Tela404";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
@@ -16,10 +12,6 @@ function App() {
         <Routes>
           <Route path="/patrimonio" element={<TelaCadastroPatrimonio/>}/>
           <Route path="/categoria" element={<TelaCadastrocategoria/>}/> 
-          <Route path="/funcao" element={<TelaCadastrofuncao/>}/>
-          <Route path="/evento" element={<TelaCadastroevento/>}/>         
-          <Route path="/pessoas" element={<TelaCadPessoa/>}/>
-          <Route path="/financas" element={<TelaCadastroFinanceiro/>}/>
           <Route path="/" element={<TelaMenu/>}/>
           <Route path="*" element={<Tela404/>} />
             
