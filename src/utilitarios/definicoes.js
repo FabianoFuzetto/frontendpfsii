44553c50911f6dc3c18e60fa3a4cbaaf1d642644
Fabiano Fuzetto <fabianofuzetto@gmail.com>
2023-08-27 00:11:21 -0300

    .

--- src/utilitarios/definicoes.js
+++ src/utilitarios/definicoes.js
@@ -1,1 +1,1 @@
-export const urlBase = "https://aluno14-pfsii.fake.edu.br/apihttps://129.146.68.51/aluno14-pfsii/";
+export const urlBase = "https://aluno14-pfsii.fake.edu.br/apihttps://129.146.68.51/aluno14-pfsii/"; 
