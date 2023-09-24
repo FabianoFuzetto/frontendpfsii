d06373fdf5d7ceee85d843d21e433f384324fd66
Fabiano Fuzetto <fabianofuzetto@gmail.com>
2023-09-24 02:56:44 -0300

    .

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import TelaCadastroPatrimonio from "./telas/TelaCadastroPatrimonio.jsx";
 import TelaCadastrocategoria from "./telas/TelaCadastroCategoria.jsx";
+import TelaCadastrofuncao from "./telas/TelaCadastroFuncao.jsx";
+import TelaCadPessoa from "./telas/TelaCadPessoa.jsx";
 import TelaMenu from "./telas/TelaMenu";
 import Tela404 from "./telas/Tela404";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
