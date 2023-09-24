ded64823c9dd82aae61c0bdc94505c7e48231d97
Fabiano Fuzetto <fabianofuzetto@gmail.com>
2023-09-24 03:22:37 -0300

    fetch

--- src/formularios/FormPessoa.jsx
+++ src/formularios/FormPessoa.jsx
@@ -29,7 +29,7 @@ export default function FormPessoa(props) {
     const form = evento.currentTarget;
     if (form.checkValidity()) {
       if (!props.atualizando) {
-        fetch(urlBase + "/pessoa", {
+        fetch("https://129.146.68.51/aluno14-pfsii/pessoa", {
           method: "POST",
           headers: {
             "Content-Type": "application/json"
@@ -55,7 +55,7 @@ export default function FormPessoa(props) {
           });
       } else {
         debugger;
-        fetch(urlBase + "/pessoa", {
+        fetch("https://129.146.68.51/aluno14-pfsii/pessoa", {
           method: "PUT",
           headers: {
             "Content-Type": "application/json"
@@ -90,7 +90,7 @@ export default function FormPessoa(props) {
   }
 
   useEffect(() => {
-    fetch(urlBase + "/funcao", {
+    fetch("https://129.146.68.51/aluno14-pfsii/funcao", {
       method: "GET"
     })
       .then((resposta) => resposta.json())
