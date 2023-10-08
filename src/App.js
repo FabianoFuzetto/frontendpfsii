60693bf85aed6c2dfd73f4468b3ce72e20229f5a
Fabiano Fuzetto <fabianofuzetto@gmail.com>
2023-10-08 18:01:33 -0300

    Pesquisa

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import TelaCadPessoa from "./telas/TelaCadPessoa.jsx";
 import TelaMenu from "./telas/TelaMenu";
 import Tela404 from "./telas/Tela404";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import SearchBar from './SearchBar';
+import React, { useState } from 'react';
+
 
 
 function App() {
@@ -27,4 +30,29 @@ function App() {
   );
 }
 
+
+
+//Barra de pesquisa
+const App = () => {
+  const handleSearch = (term) => {
+    // Aqui você pode realizar a lógica de pesquisa para cada tipo de cadastro
+    console.log(`Realizando pesquisa para: ${term}`);
+  };
+
+  return (
+    <div>
+      <h1>Barra de Pesquisa Componentizada</h1>
+      <SearchBar onSearch={handleSearch} />
+      {/* Adicione aqui outros componentes ou lógica necessários para cada tipo de cadastro */}
+    </div>
+  );
+};
+
+
+
+
+
 export default App;
+
+
+
---
+++ src/Componentes/SearchBar.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+
+const SearchBar = ({ onSearch }) => {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleSearch = () => {
+    onSearch(searchTerm);
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Pesquisar..."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      <button onClick={handleSearch}>Pesquisar</button>
+    </div>
+  );
+};
+
+export default SearchBar;
