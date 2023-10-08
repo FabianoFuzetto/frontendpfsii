730a27846884720f2a0e5199a477a9b0fbb80e2a
Fabiano Fuzetto <fabianofuzetto@gmail.com>
2023-10-08 18:45:25 -0300

    Search

--- src/tabelas/TabelaFuncao.jsx
+++ src/tabelas/TabelaFuncao.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Button, Container, Table, Form, Row } from 'react-bootstrap';
-import SearchBar from './SearchBar'; // Importe o componente SearchBar
-
+import SearchBar from '../Componentes/SearchBar';
 export default function TabelaFuncao(props) {
   const [searchResults, setSearchResults] = useState(props.listaFuncao || []);
 
