3ebe0bd52b2b97dbdf638d12e5efaef73fbf8216
Fabiano Fuzetto <fabianofuzetto@gmail.com>
2023-10-09 22:00:07 -0300

    img

--- src/telas/Tela404.jsx
+++ src/telas/Tela404.jsx
@@ -1,26 +1,13 @@
-import Pagina from "../templates/Pagina";
-import frontalslide1 from "../Imagens/frontalslide1.png"
-import frontalslide2 from "../Imagens/frontalslide2.png"
-import frontalslide3 from "../Imagens/frontalslide3.png"
-import Carousel from 'react-bootstrap/Carousel';
+import { Alert } from "react-bootstrap";
 import Pagina from "../templates/Pagina";
 
 
 export default function Tela404(props) {
     return (
         <Pagina>
-            <Carousel className="w-75">
-                    <Carousel.Item>
-                    <img className="d-block w-100 shadow-lg  rounded " src={frontalslide1} alt="Primeiro slide" />  
-                    </Carousel.Item>
-                    <Carousel.Item>
-                    <img className="d-block w-100 shadow-lg  rounded " src={frontalslide2} alt="Primeiro slide" />
-                    </Carousel.Item>
-                    <Carousel.Item>
-                    <img className="d-block w-100 shadow-lg  rounded " src={frontalslide3} alt="Primeiro slide" />
-                    </Carousel.Item>
-                    {/* Adicione mais Carousel.Items conforme necessário */}
-                </Carousel>
+            <Alert className="text-center m-2" variant="warning">
+            <img className="d-block w-100 shadow-lg  rounded " src={frontalslide1} alt="Primeiro slide" />
+            </Alert>
         </Pagina>
     );
 }
